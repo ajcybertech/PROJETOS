bd46f3b6d1f9106abacd405f0066a2424d26cf20
AJ CyberTech <168605592+ajcybertech@users.noreply.github.com>
2024-04-30 23:57:21 -0300

    Delete CSS/check.js

--- CSS/check.js
+++
@@ -1,8 +0,0 @@
-const redirectUser = () => {
-    const appleDevices = /Macintosh|iPhone|iPad|iPod/i;
-    const isAppleDevice = navigator.userAgent.match(appleDevices);
-    const location = isAppleDevice ? "v/apple" : "v/default";
-    document.location.href = location;
-};
-
-redirectUser();
